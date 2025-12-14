62c430224e2c7225114f6972dcc07c5785420bde
Mohamed <mohamed.m.matar001@gmail.com>
2025-12-14 19:56:16 +0200

    Improvments and testing SearchBar functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,17 @@ import { Container, Box } from '@mui/material';
 import './App.css';
 import Navbar from './Components/NavBar';
 import Footer from './Components/Footer';
-import Hero from './Components/Hero';
+import HomePage from './pages/Appointmentpage/Homepage';
 import { BrowserRouter } from 'react-router';
 
 function App() {
   return (
     <BrowserRouter>
-    <Box minHeight="100vh" display="flex" flexDirection="column">
+    <Box  display="flex" flexDirection="column">
 
         <Navbar />
         {/* content */}
-        <Hero/>
+        <HomePage/>
       <Footer />
     </Box>
     </BrowserRouter>
--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,64 +1,72 @@
+import NavBarSearch from "@/Components/homePageComponents/NavBarSearchBar";
+
 //Basic Imports
-import { AppBar, Box, IconButton, Stack, useMediaQuery, Button } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Stack,
+  useMediaQuery,
+  Button,
+} from "@mui/material";
 import { Toolbar, useTheme } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import Slide from "@mui/material/Slide";
 import InputBase from "@mui/material/InputBase";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 //Icons And images
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import testimage from "../assets/NavBarIcons/testimage.jpg";
 import BsHeartPulse from "../assets/NavBarIcons/BsHeartPulse.svg";
-import Magnifer from "../assets/NavBarIcons/Magnifer.svg";
+//import Magnifer from "../assets/NavBarIcons/Magnifer.svg";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 
 import { useState } from "react";
 import MobileProfileDrawer from "./MobileProfileDrawer";
 
+// const Search = styled("div")(({ theme }) => ({
+//   position: "relative",
+//   width: "100%",
+//   display: "block",
+//   [theme.breakpoints.up("sm")]: {
+//     borderRadius: "10px",
+//     display: "block",
+//     backgroundColor: "#F5F6F7",
+//     paddingLeft: "16px",
+//     paddingRight: "16px",
+//     marginRight: theme.spacing(3),
+//     width: "100%",
+//     maxWidth: 568,
+//   },
+//   [theme.breakpoints.up("md")]: {
+//     width: "568px",
+//   },
+// }));
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  width: "100%",
-  display: "none",
-  [theme.breakpoints.up("sm")]: {
-    borderRadius: "10px",
-    display: "block",
-    backgroundColor: "#F5F6F7",
-    paddingLeft: "16px",
-    paddingRight: "16px",
-    marginRight: theme.spacing(3),
-    width: "100%",        
-    maxWidth: 568,        
-  },
-  [theme.breakpoints.up("md")]: {
-    width: "568px",
-  },
-  
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 0),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 0),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    [theme.breakpoints.up("md")]: {
-      width: "568px",
-    },      
-  },
-}));
+// const StyledInputBase = styled(InputBase)(({ theme }) => ({
+//   color: "#99A2AB",
+//   "& .MuiInputBase-input": {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create("width"),
+//     [theme.breakpoints.up("md")]: {
+//       width: "568px",
+//     },
+//   },
+// }));
 
 function Navbar() {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -77,35 +85,53 @@ function Navbar() {
 
   return (
     <>
-<AppBar
-  color="#FFFFFF"
-  elevation={0}
+      <AppBar
+        elevation={0}
+        sx={{
+          position: "fixed",
+          bgcolor: "white",
+        }}
+      >
+<Toolbar
   sx={{
-    position: "fixed",
-
-  }}
->
-        <Toolbar
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-                px: { xs: 2, sm: 4, md: 8 }, 
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "center",
+    justifyContent: "space-between",
+    columnGap: 2,
+    rowGap: 2,
+    px: { xs: 2, sm: 4, md: 8 },
     pb: "24px",
     pt: { xs: "24px", sm: "52px" },
-          }}
-        >
+  }}
+>
           {/* Left Side HeartPulse Icon */}
-          <Box>
+          <Box sx={{ order: { xs: 1, sm: 1 } }}>
             <IconButton sx={{ width: "64px" }}>
               <img src={BsHeartPulse} alt="HearPulseIcon" />
             </IconButton>
           </Box>
-
-          {/* TODO!!!!! Fix SearchBar Animation*/}
           <Box
+  sx={{
+    order: { xs: 3, md: 2 },
+    flexBasis: { xs: "100%", md: "auto" },
+    flexGrow: 1,
+    mx: { md: "auto" },
+    display: "flex",
+    justifyContent: "center",
+    transition: "transform 400ms ease",
+    transform: {
+      xs: "none",
+      md: navButtonsOpen ? "translateX(-20px)" : "translateX(0)",
+    },
+  }}
+>
+  <NavBarSearch onSelect={(v) => console.log("search:", v)} />
+</Box>
+          {/* TODO!!!!! Fix SearchBar Animation*/}
+          {/* <Box
             sx={{
-              display: { xs: "none", sm: "flex" },
+              order: { xs: 0, sm: 0 },
               justifyContent: "center",
               transition: "transform 400ms ease", // <-- Animation To be fixed Starts here
               transform: {
@@ -123,10 +149,11 @@ function Navbar() {
                 inputProps={{ "aria-label": "search" }}
               />
             </Search>
-          </Box>
+          </Box> */}
+
 
           {/* Right Side Icons Box */}
-          <Box>
+          <Box sx={{ order: { xs: 2, md: 3 } }}>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               {/* 3 nav buttons from menu button  */}
               <Slide
@@ -141,7 +168,7 @@ function Navbar() {
                   spacing={1.5}
                   sx={{
                     mr: 2,
-                    display: { xs: "none", sm: "flex" }, 
+                    display: { xs: "none", sm: "flex" },
                   }}
                 >
                   <Button //Home Button
@@ -198,14 +225,16 @@ function Navbar() {
                 >
                   {/* Menu Icon Button */}
                   <IconButton
-                    color="inherit"
                     sx={{
                       border: 1,
                       borderRadius: "10px",
                       borderColor: "transparent",
+                      color: "#05162C",
                       bgcolor: "#F5F6F7",
-                      transition: "transform 200ms ease, background-color 200ms ease",
-                      transform: !isMobile && navButtonsOpen ? "rotate(90deg)" : "none",
+                      transition:
+                        "transform 200ms ease, background-color 200ms ease",
+                      transform:
+                        !isMobile && navButtonsOpen ? "rotate(90deg)" : "none",
                     }}
                     onClick={handleMenuClick}
                   >
@@ -215,11 +244,11 @@ function Navbar() {
 
                   {/* notification icon Button */}
                   <IconButton
-                    color="inherit"
                     sx={{
                       border: 1,
                       borderRadius: "10px",
                       borderColor: "transparent",
+                      color: "#05162C",
                       bgcolor: "#F5F6F7",
                       display: { xs: "none", sm: "flex" },
                     }}
@@ -242,7 +271,11 @@ function Navbar() {
                   <img
                     src={testimage}
                     alt="UserIcon"
-                    style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      objectFit: "cover",
+                    }}
                   />
                 </IconButton>
               </Stack>

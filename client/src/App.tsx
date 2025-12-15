201220a0ae89aa423096a3edb01dcc6d2361e512
Marioo-Ayman <96667299+Marioo-Ayman@users.noreply.github.com>
2025-12-15 21:48:28 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,18 +4,25 @@ import Navbar from './Components/NavBar';
 import Footer from './Components/Footer';
 import HomePage from './pages/Appointmentpage/Homepage';
 import { BrowserRouter } from 'react-router';
+import { Route, Routes } from "react-router-dom"
+import LogIn from "./auth/LogIn"
+import SignUp from "./auth/SignUp"
+import Verification from "./auth/Verification"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
+import Profile from "./pages/Profile"
 
 function App() {
   return (
-    <BrowserRouter>
-    <Box  display="flex" flexDirection="column">
-
         <Navbar />
-        {/* content */}
+    <Routes>
+      <Route path="/login" element={<LogIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/verify" element={<Verification/>}/>
+      <Route path="/privacy" element={<PrivacyPolicy/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
         <HomePage/>
       <Footer />
-    </Box>
-    </BrowserRouter>
   );
 }
 
